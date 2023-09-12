daaff56da3d0dd28f79581603e0c2deb7fb5ca53
philip-dluhc <philip.sinfield@levellingup.gov.uk>
2023-09-12 15:37:08 +0100

    Dev: Updated handling of optional welsh name in validators

--- geopkg/validators.py
+++ geopkg/validators.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from geopandas import GeoDataFrame
 
 from geopkg.constants import GEOMETRY_COLUMN_NAME
@@ -5,9 +7,12 @@ from geopkg.exceptions import InvalidGeoDataFrameStructure
 
 
 def validate_geocode_gdf(
-    geo: GeoDataFrame, code: str, name: str, welsh_name: str
+    geo: GeoDataFrame, code: str, name: str, welsh_name: Optional[str]
 ) -> None:
-    columns: tuple[str, ...] = GEOMETRY_COLUMN_NAME, code, name, welsh_name
+
+    columns: list[str] = [
+        c for c in [GEOMETRY_COLUMN_NAME, code, name, welsh_name] if c is not None
+    ]
 
     if missing := [*filter(lambda c: c not in geo.columns, columns)]:
         raise InvalidGeoDataFrameStructure(

1b251e659a3a01c190ef4f1e3113a8cdbf6d3f38
ellsphillips <elliott.phillips.dev@gmail.com>
2023-09-12 11:37:06 +0100

    Feature: uk bbox and crs constants

--- geopkg/constants.py
+++ geopkg/constants.py
@@ -1,2 +1,21 @@
+from enum import Enum
+
+from geopkg.types import BoundingBox
+
 GEOMETRY_COLUMN_NAME = "geometry"
 REQUIRED_COLUMNS: tuple[str, ...] = GEOMETRY_COLUMN_NAME, "code", "name", "welsh_name"
+
+DEFAULT_MAX_ZOOM = 14
+
+
+class CoordinateReferenceSystem(Enum):
+    WGS84 = 4326
+    WEB_MERCATOR = 3857
+
+
+UK_BBOX = BoundingBox(
+    east=1.9167,
+    north=60.8333,
+    south=49.8333,
+    west=-8.1667,
+)
